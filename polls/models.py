be351b3b8a1441aa8e871f21f8b336a92059842f
yuukirintaynnyuuto <miki140518@gmail.com>
2024-10-16 12:15:31 +0900

    django finish

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,9 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+from django.http import HttpResponse
+from django.contrib import admin
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -11,8 +14,17 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 
 class Choice(models.Model):
@@ -22,3 +34,9 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
